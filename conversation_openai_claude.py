533d76b330ed383895e04a8fce98341400147e90
Volodymyr Velichko <vova.box.new@gmail.com>
2025-03-26 22:10:57 +0200

    conversation between two models with different characters, defined in system prompts

---
+++ conversation_openai_claude.py
@@ -0,0 +1,89 @@
+# imports
+
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import anthropic
+
+
+# Load environment variables in a file called .env
+# Print the key prefixes to help with any debugging
+
+load_dotenv(override=True)
+openai_api_key = os.getenv('OPENAI_API_KEY')
+anthropic_api_key = os.getenv('ANTHROPIC_API_KEY')
+
+
+if openai_api_key:
+    print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
+else:
+    print("OpenAI API Key not set")
+    
+if anthropic_api_key:
+    print(f"Anthropic API Key exists and begins {anthropic_api_key[:7]}")
+else:
+    print("Anthropic API Key not set")
+
+
+# Connect to OpenAI, Anthropic
+openai = OpenAI()
+claude = anthropic.Anthropic()
+
+
+# Let's make a conversation between GPT-4o-mini and Claude-3-haiku
+# We're using cheap versions of models so the costs will be minimal
+gpt_model = "gpt-4o-mini"
+claude_model = "claude-3-haiku-20240307"
+
+gpt_system = "You are a chatbot who is very argumentative; \
+you disagree with anything in the conversation and you challenge everything, in a snarky way."
+
+claude_system = "You are a very polite, courteous chatbot. You try to agree with \
+everything the other person says, or find common ground. If the other person is argumentative, \
+you try to calm them down and keep chatting."
+
+
+# asks gpt function
+def call_gpt():
+    messages = [{"role": "system", "content": gpt_system}]
+    for gpt, claude in zip(gpt_messages, claude_messages):
+        messages.append({"role": "assistant", "content": gpt})
+        messages.append({"role": "user", "content": claude})
+    completion = openai.chat.completions.create(
+        model=gpt_model,
+        messages=messages
+    )
+    return completion.choices[0].message.content
+
+
+# ask claude function
+def call_claude():
+    messages = []
+    for gpt, claude_message in zip(gpt_messages, claude_messages):
+        messages.append({"role": "user", "content": gpt})
+        messages.append({"role": "assistant", "content": claude_message})
+    messages.append({"role": "user", "content": gpt_messages[-1]})
+    message = claude.messages.create(
+        model=claude_model,
+        system=claude_system,
+        messages=messages,
+        max_tokens=500
+    )
+    return message.content[0].text
+
+
+# Run a small conversation
+gpt_messages = ["Hi there"]
+claude_messages = ["Hi"]
+
+print(f"GPT:\n{gpt_messages[0]}\n")
+print(f"Claude:\n{claude_messages[0]}\n")
+
+for i in range(3):
+    gpt_next = call_gpt()
+    print(f"GPT:\n{gpt_next}\n")
+    gpt_messages.append(gpt_next)
+    
+    claude_next = call_claude()
+    print(f"Claude:\n{claude_next}\n")
+    claude_messages.append(claude_next)
